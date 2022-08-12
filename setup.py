d53340be7c235151c705ce3b8efc237797ea8a08
Iwan Paolucci <iwan.paolucci@gmail.com>
2022-08-12 14:11:39 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pmsurv',
+    version='0.1.0',
+    packages=['pmsurv'],
+    package_dir={'': 'pmsurv'},
+    url='',
+    license='MIT',
+    author='ipaolucci',
+    author_email='',
+    description='Pymc survival'
+)
