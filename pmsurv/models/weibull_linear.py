755c9ae95832b78aa1c0aeb0d97f4118092dccf4
Iwan Paolucci <iwan.paolucci@gmail.com>
2022-10-28 08:57:35 -0500

    improved compilation for weibul linear

--- pmsurv/models/weibull_linear.py
+++ pmsurv/models/weibull_linear.py
@@ -48,36 +48,38 @@ class WeibullModelLinear(WeibullModelBase):
         with model:
             if X is None:
                 print("create cached with empty data")
-                model_input = pm.MutableData("model_input", np.zeros([self.num_training_samples, self.num_pred]))
-                time_censor_ = pm.MutableData("time_censor",
-                                              np.zeros(np.ceil(self.num_training_samples / 2).astype(int)))
-                time_uncensor_ = pm.MutableData("time_uncensor",
-                                                np.zeros(np.floor(self.num_training_samples / 2).astype(int)))
-                censor_ = pm.MutableData("censor", default_rng(seed=0).choice([1, 0], size=(self.num_training_samples),
-                                                                              p=[0.5, 0.5]).astype(np.int32))
-            else:
-                print("create cached with real data")
-                model_input = pm.MutableData("model_input", X)
+                X = np.zeros([self.num_training_samples, self.num_pred])
+
+            model_input = pm.MutableData("model_input", X)
+            if y is not None:
                 time_censor_ = pm.MutableData("time_censor", y[y[:, 1] == 1, 0])
                 time_uncensor_ = pm.MutableData("time_uncensor", y[y[:, 1] == 0, 0])
                 censor_ = pm.MutableData("censor", y[:, 1].astype(np.int8))
-                # print(censor_.dtype)
 
-        with model:
             print("Priors: {}".format(str(self.priors)))
 
             lambda_intercept = pm.Normal("lambda_intercept",
-                                         mu=self.priors['lambda_mu'], sigma=self.priors['lambda_sd'])
+                                         mu=self.priors['lambda_mu'],
+                                         sigma=self.priors['lambda_sd'])
 
             k_intercept = pm.Normal('k_intercept',
-                                    mu=self.priors['k_mu'], sigma=self.priors['k_sd'])
+                                    mu=self.priors['k_mu'],
+                                    sigma=self.priors['k_sd'])
+
+            print('making lambda')
             lambda_coefs = []
             for i, cn in enumerate(self.column_names):
+                print('add ', cn)
                 lambda_coef = pm.Normal(f'lambda_{cn}',
                                         mu=self.priors['lambda_coefs_mu'],
                                         sigma=self.priors['lambda_coefs_sd'])
                 lambda_coefs.append(model_input[:, i] * lambda_coef)
-            lam = sum(lambda_coefs)
+            lam = pm.math.sum(lambda_coefs, axis=0)
+            print('summed')
+            # print(lam.shape.eval())
+            # print(lam.eval())
+            # lambda_coefs = pm.Normal('lambda_coefs', mu=self.priors['lambda_coefs_mu'],
+            #                          sd=self.priors['lambda_coefs_sd'], shape=(self.num_pred,))
 
             if self.priors['k_coefs']:
                 k_coefs = []
@@ -86,23 +88,29 @@ class WeibullModelLinear(WeibullModelBase):
                                             mu=self.priors['k_coefs_mu'],
                                             sigma=self.priors['k_coefs_sd'])
                     k_coefs.append(model_input[:, i] * k_coef)
-                    k = sum(k_coefs)
+                    k = pm.math.sum(k_coefs)
             else:
-                k = (0.0 * model_input).sum(axis=1)
+                k = pm.math.zeros_like(lam) #0.0 * model_input).sum(axis=1)
 
+            print('ks done')
+
+            # lambda_ = pm.Deterministic("lambda_det",
+            #                            pm.math.exp(lambda_intercept + (lambda_coefs * model_input).sum(axis=1)))
             lambda_ = pm.Deterministic("lambda_det",
                                        pm.math.exp(lambda_intercept + lam))
             k_ = pm.Deterministic("k_det", pm.math.exp(k_intercept + k))
 
-            censor_ = at.eq(censor_, 1)
-            y = pm.Weibull("y", alpha=k_[~censor_], beta=lambda_[~censor_],
-                           observed=time_uncensor_)
+            if y is not None:
+                censored_ = at.eq(censor_, 1)
+                # print(k_.shape.eval(), lambda_.shape.eval(), censored_.shape.eval())
+                y = pm.Weibull("y", alpha=k_[~censored_], beta=lambda_[~censored_],
+                               observed=time_uncensor_)
 
-            def weibull_lccdf(x, alpha, beta):
-                """ Log complementary cdf of Weibull distribution. """
-                return -((x / beta) ** alpha)
+                def weibull_lccdf(x, alpha, beta):
+                    """ Log complementary cdf of Weibull distribution. """
+                    return -((x / beta) ** alpha)
 
-            y_cens = pm.Potential("y_cens", weibull_lccdf(time_censor_, alpha=k_[censor_], beta=lambda_[censor_]))
+                y_cens = pm.Potential("y_cens", weibull_lccdf(time_censor_, alpha=k_[censored_], beta=lambda_[censored_]))
 
         return model
 
