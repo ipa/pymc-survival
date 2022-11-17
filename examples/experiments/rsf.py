34b889058635fbba5d457c33b1fa1b3125c277f2
Iwan Paolucci <iwan.paolucci@gmail.com>
2022-11-17 13:37:31 -0600

    refactored running experiments

--- examples/experiments/rsf.py
+++ examples/experiments/rsf.py
@@ -1,60 +1,20 @@
-import warnings
-
-import joblib
-import matplotlib
-import argparse
-import os
-
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline
 from sksurv.datasets import get_x_y
 from sksurv.ensemble import RandomSurvivalForest
-from sksurv.metrics import as_concordance_index_ipcw_scorer
-
-from lifelines.utils import concordance_index
-
-import logging
-import time
-import utils
-import pandas as pd
-from sklearn.model_selection import train_test_split, GridSearchCV, KFold
-from sklearn.feature_selection import mutual_info_classif
-from sklearn.preprocessing import StandardScaler
-import yaml
-from tqdm import tqdm
-import numpy as np
-from scipy import stats as sp
-matplotlib.use('Agg')
-from skopt import BayesSearchCV
 from skopt.space import Real, Categorical, Integer
-
-localtime = time.localtime()
-TIMESTRING = time.strftime("%m%d%Y%M", localtime)
-
-DURATION_COL = 'time'
-EVENT_COL = 'censor'
+import utils
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('experiment', help='name of the experiment that is being run')
-    parser.add_argument('dataset', help='.h5 File containing the train/valid/test datasets')
-    parser.add_argument('--results_dir', default='results',
-                        help='Directory to save resulting models and visualizations')
-    parser.add_argument('--runs', type=int, default=1)
-    parser.add_argument('--jobs', type=int, default=1)
-    return parser.parse_args()
+def preprocess_data(dataset, config):
+    X = dataset[config['features']]
+    _, y = get_x_y(dataset,
+                   attr_labels=[config['outcome']['event'], config['outcome']['time']],
+                   pos_label=config['outcome']['pos_label'])
+    return X, y
 
 
-def run_rsf(X, y, config, kwargs):
-    X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                        test_size=config['split']['test_size'],
-                                                        shuffle=config['split']['shuffle'])
-
-    if config['preprocessing']['standardize']:
-        logger.info("Standardize data")
-        X_train, X_test = utils.standardize(X_train, X_test, config)
-
+def train_model(X_train, y_train, config, train_kwargs):
     pipeline = Pipeline(
         [
             ('selector', SelectKBest(utils.mutual_info_surv)),
@@ -68,48 +28,6 @@ def run_rsf(X, y, config, kwargs):
         'selector__k': Integer(1, X_train.shape[1]),
     }
 
-    jobs = 1 if 'jobs' not in kwargs else kwargs['jobs']
-    opt = BayesSearchCV(pipeline, parameters, n_jobs=jobs, n_points=2, n_iter=50, cv=5)
-    opt.fit(X_train, y_train)
+    fit_params = {}
 
-    metrics = opt.best_estimator_.score(X_test, y_test)
-    logger.info("Test metrics: " + str(metrics))
-
-    return metrics, opt.best_estimator_, opt.best_params_, (X_train, X_test, y_train, y_test)
-
-
-if __name__ == '__main__':
-    logger = logging.getLogger(__name__)
-    logging.basicConfig(level=logging.WARNING)
-    args = parse_args()
-
-    logger.info("Running {0} experiment".format(args.experiment))
-    # logger.info("Arguments:", str(args))
-
-    # Load Dataset
-    logger.info("Loading datasets: " + args.dataset)
-    dataset, config = utils.load_data(args.dataset)
-
-    X = dataset[config['features']]
-    _, y = get_x_y(dataset,
-                   attr_labels=[config['outcome']['event'], config['outcome']['time']],
-                   pos_label=config['outcome']['pos_label'])
-
-    logger.info(f"Running for {args.runs} runs..")
-    pbar = tqdm(range(args.runs))
-
-    experiment_dir = os.path.join(args.results_dir, args.experiment)
-    os.makedirs(experiment_dir, exist_ok=True)
-
-    c_indexes = utils.RollingMean()
-    warnings.filterwarnings(action='ignore')
-    for run in pbar:
-        c_index, model, params, data = run_rsf(X, y, config, {'jobs': args.jobs})
-        c_indexes.add(c_index)
-        utils.save_results(args.results_dir, 'rsf', args.dataset, c_index, params)
-        pbar.set_description(f"C-Index {str(c_indexes)}")
-
-        if c_index >= c_indexes.get_max():
-            data_dump = {'model': model, 'params': params, 'data': data}
-            joblib.dump(data_dump, os.path.join(experiment_dir, f"rsf_best.pkl"))
-
+    return pipeline, parameters, fit_params
---
+++ examples/experiments/run_experiment.py
@@ -0,0 +1,118 @@
+import argparse
+import logging
+import time
+import warnings
+import sys
+import contextlib
+import os
+import joblib
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from skopt import BayesSearchCV
+import utils
+import pmsurv_exponential
+import rsf
+
+localtime = time.localtime()
+TIMESTRING = time.strftime("%m%d%Y%M", localtime)
+
+
+class DummyFile(object):
+    def write(self, x): pass
+
+
+@contextlib.contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    sys.stdout = DummyFile()
+    yield
+    sys.stdout = save_stdout
+
+
+preprocess_data_fun = {
+    'pmsurv_exponential': pmsurv_exponential.preprocess_data,
+    'rsf': rsf.preprocess_data
+}
+
+train_fun = {
+    'rsf': rsf.train_model,
+    'pmsurv_exponential': pmsurv_exponential.train_model
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('model', type=str)
+    parser.add_argument('experiment', help='name of the experiment that is being run')
+    parser.add_argument('dataset', help='.h5 File containing the train/valid/test datasets')
+    parser.add_argument('--results_dir', default='results',
+                        help='Directory to save resulting models and visualizations')
+    parser.add_argument('--runs', type=int, default=1)
+    parser.add_argument('--jobs', type=int, default=1)
+    parser.add_argument('--n-iter', type=int, default=25)
+    return parser.parse_args()
+
+
+def run_experiment(model, dataset, config, train_kwargs):
+    X, y = preprocess_data_fun[model](dataset, config)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=config['split']['test_size'],
+                                                        shuffle=config['split']['shuffle'])
+
+    if config['preprocessing']['standardize']:
+        logger.info("Standardize data")
+        X_train, X_test = utils.standardize(X_train, X_test, config)
+
+    pipeline, parameters, fit_params = train_fun[model](X_train, y_train, config, train_kwargs)
+
+    opt = BayesSearchCV(pipeline, parameters,
+                        fit_params=fit_params,
+                        n_jobs=train_kwargs['jobs'],
+                        n_points=1,
+                        n_iter=train_kwargs['n_iter'],
+                        cv=5)
+    opt.fit(X_train, y_train)
+
+    metrics = opt.best_estimator_.score(X_test, y_test)
+    logger.info("Test metrics: " + str(metrics))
+
+    return metrics, opt.best_estimator_, opt.best_params_, (X_train, X_test, y_train, y_test)
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.WARNING)
+    logger_pymc = logging.getLogger("pymc")
+    logger_pymc.setLevel(logging.ERROR)
+    logger_pymc.propagate = False
+
+    args = parse_args()
+
+    logger.info("Running {0} experiment".format(args.experiment))
+
+    # Load Dataset
+    logger.info("Loading datasets: " + args.dataset)
+    dataset, config = utils.load_data(args.dataset)
+
+    logger.info(f"Running for {args.runs} runs..")
+    pbar = tqdm(range(args.runs))
+
+    experiment_dir = os.path.join(args.results_dir, args.experiment)
+    os.makedirs(experiment_dir, exist_ok=True)
+
+    train_kwargs = {'jobs': args.jobs, 'n_iter': args.n_iter}
+
+    c_indexes = utils.RollingMean()
+    warnings.filterwarnings(action='ignore')
+
+    for run in pbar:
+        with nostdout():
+            c_index, model, params, data = run_experiment(args.model, dataset, config, train_kwargs)
+        c_indexes.add(c_index)
+        utils.save_results(args.results_dir, args.model, args.dataset, c_index, params)
+        pbar.set_description(f"C-Index {str(c_indexes)}")
+
+        # if c_index >= c_indexes.get_max():
+        #     data_dump = {'model': model, 'params': params, 'data': data}
+            # joblib.dump(data_dump, os.path.join(experiment_dir, f"{args.model}_best.pkl"))
