413299e46215574dc3e673ab608f3e0409e14ec8
Iwan Paolucci <iwan.paolucci@gmail.com>
2022-12-01 09:47:17 -0600

    deepsurv for continous learning

--- examples/experiments/deepsurv.py
+++ examples/experiments/deepsurv.py
@@ -22,26 +22,29 @@ class PyCoxWrapper(BaseEstimator):
         self.hidden_units = hidden_units
         self.dropout = dropout
         self.lr = lr
-        self.net = None
+        self.net = None 
         self.cox = None
 
     def fit(self, X, y, batch_size=512, epochs=200):
         X = X.astype(np.float32)
         y = y.astype(np.float32)
+        print(X.dtype, y.dtype)
         y = (y[:, 0], y[:, 1])
 
         # self.hidden_units = [X.shape[1], X.shape[1]]
         #if batch_size > (X.shape[0] / 2):
         #    batch_size = 128
-            
-        self.net = tt.practical.MLPVanilla(in_features=X.shape[1],
+        if self.cox is None:
+            print('learn from scratch')
+            self.net = tt.practical.MLPVanilla(in_features=X.shape[1],
                                       num_nodes=[self.hidden_units],
                                       out_features=1,
                                       batch_norm=True,
                                       dropout=self.dropout,
                                       output_bias=False)
-        self.cox = CoxPH(self.net, tt.optim.Adam(1e-2))
-        self.cox.optimizer.set_lr(self.lr)
+            self.cox = CoxPH(self.net, tt.optim.Adam(1e-2))
+            self.cox.optimizer.set_lr(self.lr)
+
         log = self.cox.fit(X, y, batch_size, epochs,
                            verbose=False)
         _ = self.cox.compute_baseline_hazards()
@@ -88,6 +91,18 @@ def train_model(X_train, y_train, config, train_kwargs):
     fit_params = {}
 
     return pipeline, parameters, fit_params
+
+
+def retrain_model(X_train, y_train, config, train_kwargs, prior_model=None):
+    import copy
+
+    if prior_model is None:
+        model = PyCoxWrapper(hidden_units=X_train.shape[1])
+    else:
+        model = copy.deepcopy(prior_model)
+    
+    model.fit(X_train.values, y_train)
+    return model
 
 
 def save(save_dir, model, c_index, params, data):
