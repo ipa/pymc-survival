5811bf74d4bf8117f30923cdcab4abd5db0e4438
Iwan Paolucci <592707+ipa@users.noreply.github.com>
2022-12-14 17:33:37 -0600

    prior from posterior (not working)

---
+++ pmsurv/models/priors.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy import stats
+from pymc.distributions import Interpolated, Normal
+import pymc as pm
+
+def from_posterior(param, samples):
+    smin, smax = np.min(samples), np.max(samples)
+    width = smax - smin
+    x = np.linspace(smin, smax, 100)
+    y = np.linspace(smin, smax, 100) #stats.gaussian_kde(samples)(x)
+
+    # what was never sampled should have a small probability but not 0,
+    # so we'll extend the domain and use linear approximation of density on it
+    x = np.concatenate([[x[0] - 3 * width], x, [x[-1] + 3 * width]])
+    y = np.concatenate([[0], y, [0]])
+
+    return pm.Interpolated(param, x, y)
